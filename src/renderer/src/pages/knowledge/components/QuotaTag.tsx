eccd6acaf40d11da158133a974a9af06142e2bb7
eeee0717 <chentao020717Work@outlook.com>
2025-07-07 17:03:44 +0800

    [1.5.0-rc.5] fix: QuotaTag循环调用

--- src/renderer/src/pages/knowledge/components/QuotaTag.tsx
+++ src/renderer/src/pages/knowledge/components/QuotaTag.tsx
@@ -43,7 +43,7 @@ const QuotaTag: FC<{ base: KnowledgeBase; providerId: string; quota?: number }> 
       return
     }
     checkQuota()
-  }, [_quota, base, provider.id, provider.apiKey, provider, quota, updateProvider])
+  }, [_quota, base, provider.id, provider.apiKey])
 
   return (
     <>
